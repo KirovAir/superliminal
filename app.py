ca1d862ab157f5ff94c8086482a8dd0f865fbcb9
Jesse <info@jessesander.nl>
2018-01-04 20:47:30 +0100

    Some logging improvements.

--- app.py
+++ app.py
@@ -20,53 +20,53 @@ if not os.path.exists(CFG_FILE):
         'radarr':None}
         outfile.write(json.dumps(data, indent=4))
 
-logging.basicConfig(filename=CFG_DIR + 'web.log',level=logging.DEBUG)
+logging.basicConfig(filename=CFG_DIR + 'web.log',level=logging.INFO if not os.environ.get('DEBUG') else logging.DEBUG)
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
     if not request.json:
+        logging.debug('Skipping, request is not JSON.')
         abort(400)
-    eventype = request.json['eventType']
 
-    logging.debug(request.data)
-    if eventype == 'Test':
+    logging.debug('Got json request: ' + request.data.decode("utf-8"))
+    
+    eventType = request.json.get('eventType')
+    if eventType == 'Test':
         return 'Test'
     
     mediaFile = ''
     isMovie = False
     if request.json.get('movieFile') and request.json.get('movie'):
         isMovie = True
-        logging.info('Got moviefile!')
         mediaFile = os.path.join(request.json['movie']['folderPath'], request.json['movieFile']['relativePath'])
     if request.json.get('episodeFile') and request.json.get('series'):
-        logging.info('Got episodefile!')
         mediaFile = os.path.join(request.json['series']['path'], request.json['episodeFile']['relativePath'])
-
+        
     if not mediaFile:
-        logging.warn('No mediafile found in request')
-
-    logging.debug('Mediafile: ' + mediaFile)
-
-    data = json.load(open(CFG_FILE))
+        logging.warn('No media file found in request')
+        abort(400)
     
+    logging.info('Got ' + ('movie' if isMovie else 'episode') + ' download request! Running Subliminal for file: ' + mediaFile)
+    
+    data = json.load(open(CFG_FILE))
     cmd = data.get('radarr') if isMovie else data.get('sonarr')
     if not cmd:
         cmd = data.get('default')
 
     if not cmd:
-        logging.warn('cmd line is empty, aborting')
+        logging.warn('Parameter line is empty, please check your config..')
         abort(400)
 
     cmd = 'subliminal ' + cmd.replace('#FILE#', mediaFile)
     logging.debug('CMD: ' + cmd)
-    #os.system(cmd)
     
     try:
         with open(SUBLIMINAL_LOG, 'a') as outfile:
             subprocess.call(cmd, stdout=outfile, stderr=outfile, shell=True)
     except Exception as e:
         logging.error(e)
+
     return 'Ok :)'
 
 if __name__ == '__main__':
